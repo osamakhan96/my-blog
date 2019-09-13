c85c0e0050859fc91d494a136679b7de0e93317d
Osama Khan <hs110.work@gmail.com>
2019-09-13 05:57:18 -0700

    First commit

---
+++ src/build/precache-manifest.3795c5a703f23922d1a124095b78de69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16956f420bd3ddb24b49e4f7c047190a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "180a5e7fe38c5e71be58",
+    "url": "/static/css/main.6b933f4f.chunk.css"
+  },
+  {
+    "revision": "02daf4e70de2e911bf3b",
+    "url": "/static/js/2.4d5b4a71.chunk.js"
+  },
+  {
+    "revision": "180a5e7fe38c5e71be58",
+    "url": "/static/js/main.1e65c2bf.chunk.js"
+  },
+  {
+    "revision": "eeab855a9420d0df9e27",
+    "url": "/static/js/runtime~main.a3235f1f.js"
+  }
+]);
